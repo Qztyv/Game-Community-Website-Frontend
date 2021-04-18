816d9880f02eaeed9e690b872d4ebc74bd410409
Qztyv <nickocowling@gmail.com>
2021-04-18 19:10:53 +0100

    changed axios for production backend url...

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import store from "@/store/index.js";
 
 const apiClient = axios.create({
-  //baseURL: "https://game-community-website-backend.herokuapp.com/api/v1/", // for deployment
-  baseURL: "http://127.0.0.1:3000/api/v1/", // for local development
+  baseURL: "https://game-community-website-backend.herokuapp.com/api/v1/", // for deployment
+  //baseURL: "http://127.0.0.1:3000/api/v1/", // for local development
   timeout: 15 * 1000,
   withCredentials: true,
   headers: { "X-Forwarded-Proto": "https" },
